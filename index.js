62bbb5c4fdf43244d62df0dd4a403444debfb55b
Mukul <Mukulprajapati1319@gmail.com>
2025-02-06 00:49:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+// // // // let p = {name:"csddss"};
+// // // // let q = p;
+
+// // // // p.name="ddddd";
+// // // // console.log(p);
+// // // // console.log(q);
+
+
+// // // // let arr = ['dddd','dwdw',4,null];
+// // // // console.log(arr[3]);
+
+
+// // // // function createCourse(course_name){
+// // // //     console.log('creating ' + course_name);
+// // // // }
+
+// // // // createCourse("hhhh");
+// // // // createCourse("rrrr");
+
+// // // // function sum(a,b){
+// // // //     return a+b;
+// // // // }
+
+// // // let sum = function(a,b){
+// // //     return a+b;
+// // // }
+
+// // // let sum2 = (a,b) => {
+// // //     return a+b;
+// // // }
+
+// // // let diff = (a,b) => a-b;
+
+
+// // // console.log(asum(2,3));
+
+// // // function outer(){
+// // //     function anotherfunc(){
+// // //         console.log("Hello");
+// // //     }
+// // //     return anotherfunc;
+// // // }
+
+// // // let result=outer();
+// // // console.log(result);
+
+// // function fetchdata(callback){
+// //     setTimeout(() => {
+// //         let data = "this is to be fetched data";
+// //         callback(data, null)
+// //     }, 0.00000000001);
+// // }
+
+// // function handleData(data, error){
+// //     if(error){
+// //         console.error(error);
+// //     }
+// //     else{
+// //         console.log(data);
+// //     }
+// // }
+
+// // fetchdata(handleData)
+
+
+// getData()
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch (error => {
+//         console.error(error);
+//     })
+
+
+const tinderUser = new Object();
+console.log(tinderUser);
---
+++ objects.js
@@ -0,0 +1,26 @@
+//declaring objects
+
+const newUser = Object(); // this is singleton object
+
+newUser.id=23232;
+newUser.name="Mukul";
+newUser.age=23;
+// console.log(newUser);
+
+// const newUserr={
+
+// } // this is not singleton...multiton
+
+const newObject = {
+    email: "kgnefk@gmail.com",
+    firstname:{
+        username:"mmmmmm",
+        id:"90ab"
+    },
+
+    phone: 9090909009090
+
+
+}
+
+console.log(newObject.firstname.username);
